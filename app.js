cdfe91f025f89368dea6c42e259e3bd5e37f8bce
tobiasromaniuk <44512715+tobiasromaniuk@users.noreply.github.com>
2018-10-27 12:14:21 -0230

    Add files via upload

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const cameraView = document.querySelector("#camera--view"),
 	cameraOutput = document.querySelector("#camera--output"),
 	cameraSensor = document.querySelector("#camera--sensor"),
 	cameraTrigger = document.querySelector("#camera--trigger")
-
+// Access the device camera and stream to cameraView
 function cameraStart() {
 	navigator.mediaDevices
-	.getUserMedia(constraints)
-	.then(function(stream) {
+		.getUserMedia(constraints)
+		.then(function(stream) {
 		track = stream.getTracks()[0];
 		cameraView.srcObject = stream;
 	})
